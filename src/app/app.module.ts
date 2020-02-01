16c7051826767d4b25911e9ceb30ca93eb38c20c
llulani <luca.lulani@trilog.it>
2020-02-01 18:11:43 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxSummernoteModule } from 'ngx-summernote';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgxSummernoteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
